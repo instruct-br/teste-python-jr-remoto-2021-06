493e82d5350f2c6e11a3a807eb3ba552fdd9ed74
tkosudi <tkosudi@gmail.com>
2021-08-12 15:05:34 -0300

    tests to get the name and version of pypi API packages

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .services.api import get_package_name, get_package_versionget_package_version
 
-# Create your tests here.
+def test_connection_api_pypi_is_working_to_get_current_package_name():
+    assert get_package_name("sampleproject") == "sampleproject"
+
+def test_connection_api_pypi_is_working_to_get_current_package_version():
+    assert get_package_version("sampleproject") == "sampleproject"
