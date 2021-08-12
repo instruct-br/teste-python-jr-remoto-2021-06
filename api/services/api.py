81ed1a4c8fc95694740452f191c9a7f6fd1c5781
tkosudi <tkosudi@gmail.com>
2021-08-12 15:46:12 -0300

    test implemented to return releases

--- api/services/api.py
+++ api/services/api.py
@@ -6,7 +6,7 @@ def get_package_name(package_name: str):
     response = get(api).json()
     return response['info']['name']
 
-def get_current_package_version(package_name: str, package_version: str):
+def get_current_package_version(package_name: str):
     api = f'https://pypi.org/pypi/{package_name}/json'
     response = get(api).json()
     return response['info']['version']
--- api/tests.py
+++ api/tests.py
@@ -5,6 +5,7 @@ def test_connection_api_pypi_is_working_to_get_current_package_name():
     assert get_package_name("sampleproject") == "sampleproject"
 
 def test_connection_api_pypi_is_working_to_get_current_package_version():
-    assert get_current_package_version("sampleproject", "2.0.0") == "2.0.0"
+    assert get_current_package_version("sampleproject") == "2.0.0"
 
-
+def test_connection_api_pypi_is_working_to_get_package_releases():
+    assert get_current_package_releases("sampleproject") == ["1.0", "1.2.0", "1.3.0", "1.3.1", "2.0.0"]
